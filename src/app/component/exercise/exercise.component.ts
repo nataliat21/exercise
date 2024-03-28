fde893059d4c59a980fcd0cc8d23b8a407023f98
nataliat21 <natalia.torozco21@gmail.com>
2024-03-28 15:07:16 -0500

    The exercise component was created

---
+++ src/app/component/exercise/exercise.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'exercise-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './exercise.component.html',
+  styleUrl: './exercise.component.css'
+})
+export class ExerciseComponent {
+
+}
